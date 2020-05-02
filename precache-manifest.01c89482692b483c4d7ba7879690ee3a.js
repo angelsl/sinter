116aa6ec3a6d9fff2caf2029ec563318655c0904
angelsl <angelsl@in04.sg>
2020-05-02 23:22:52 +0800

    Fix path

---
+++ precache-manifest.01c89482692b483c4d7ba7879690ee3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "assets/icon.png"
+  },
+  {
+    "revision": "064cd838b15cf45bfb1d",
+    "url": "bundle.064cd.js"
+  },
+  {
+    "revision": "064cd838b15cf45bfb1d",
+    "url": "bundle.c1be9.css"
+  },
+  {
+    "revision": "5bb6e414708de5562fa3b7f46d9be3a0",
+    "url": "index.html"
+  },
+  {
+    "revision": "97aef0f332596789c10f",
+    "url": "polyfills.97aef.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "sw-debug.js"
+  },
+  {
+    "revision": "d948af3ca77148d39e2090dadef5d2e9",
+    "url": "sw-esm.js"
+  },
+  {
+    "revision": "277347b28a1651b19fa78450dc4cf379",
+    "url": "sw.js"
+  }
+]);
---
+++ precache-manifest.31aacc5a008a25e033ba7476ef2bf397.esm.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "assets/icon.png"
+  },
+  {
+    "revision": "064cd838b15cf45bfb1d",
+    "url": "bundle.29adf.esm.js"
+  },
+  {
+    "revision": "064cd838b15cf45bfb1d",
+    "url": "bundle.c1be9.css"
+  },
+  {
+    "revision": "5bb6e414708de5562fa3b7f46d9be3a0",
+    "url": "index.html"
+  },
+  {
+    "revision": "97aef0f332596789c10f",
+    "url": "polyfills.43c39.esm.js"
+  }
+]);
