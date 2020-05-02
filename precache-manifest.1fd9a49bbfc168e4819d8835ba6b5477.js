d96ed805d27f5efea80c397aa416831f4c966cd3
angelsl <angelsl@in04.sg>
2020-05-02 23:15:03 +0800

    Initial commit

---
+++ precache-manifest.1fd9a49bbfc168e4819d8835ba6b5477.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "6d0515d8476475170870",
+    "url": "/bundle.6d051.js"
+  },
+  {
+    "revision": "6d0515d8476475170870",
+    "url": "/bundle.c1be9.css"
+  },
+  {
+    "revision": "802f12e670352e71f149e4e2c4b4b5b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bb5bda3b3a821d57e9b",
+    "url": "/polyfills.8bb5b.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "0d6a099ba690523f3057d200f9050fc9",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
