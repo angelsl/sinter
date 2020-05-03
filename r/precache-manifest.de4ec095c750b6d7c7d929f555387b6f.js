2ea1a5c0534fc9be06c04ff694be55e28d3fcdb8
angelsl <angelsl@in04.sg>
2020-05-03 23:01:51 +0800

    Add release build

---
+++ r/precache-manifest.de4ec095c750b6d7c7d929f555387b6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "assets/icon.png"
+  },
+  {
+    "revision": "c449dee169d5e3c2b382",
+    "url": "bundle.3277c.css"
+  },
+  {
+    "revision": "c449dee169d5e3c2b382",
+    "url": "bundle.c449d.js"
+  },
+  {
+    "revision": "22df24b2e4ae2e9acd12f7a9aa7c844d",
+    "url": "index.html"
+  },
+  {
+    "revision": "97aef0f332596789c10f",
+    "url": "polyfills.97aef.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "sw-debug.js"
+  },
+  {
+    "revision": "645da56cfe35712c68dc7104b379e853",
+    "url": "sw-esm.js"
+  },
+  {
+    "revision": "277347b28a1651b19fa78450dc4cf379",
+    "url": "sw.js"
+  }
+]);
